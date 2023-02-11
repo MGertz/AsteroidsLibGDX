a4cee2337f924fc42de35c8b51574a42ed883b11
Michael Gertz <michael@ringhus.dk>
2023-02-12 00:27:05 +0100

    Create Game.java

---
+++ src/main/java/dk/ringhus/main/Game.java
@@ -0,0 +1,54 @@
+package dk.ringhus.main;
+
+import com.badlogic.gdx.ApplicationListener;
+
+public class Game implements ApplicationListener {
+
+    /*
+     * Method is called once when the game is launched.
+     */
+    @Override
+    public void create() {
+
+    }
+
+    /*
+     * Gets called when the window is resized
+     */
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    /*
+     * A method called constantly, this is where the game logic should be loaded from.
+     */
+    @Override
+    public void render() {
+
+    }
+
+    /*
+     * Used for android phones, used when an incomming call is active, then the game pauses.
+     */
+    @Override
+    public void pause() {
+
+    }
+
+    /*
+     * Used for android phones, used when an incomming call is ended, then the game unpauses.
+     */
+    @Override
+    public void resume() {
+
+    }
+
+    /*
+     * Called when the game is closed. Only called once.
+     */
+    @Override
+    public void dispose() {
+
+    }
+}
