cc3dcbdfd421bfc2eb95525b282703277af8c930
Michael Gertz <michael@ringhus.dk>
2023-02-12 00:27:02 +0100

    Moved main.java

---
+++ src/main/java/dk/ringhus/main/Main.java
@@ -0,0 +1,16 @@
+package dk.ringhus.main;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class Main {
+    public static void main(String[] args) {
+        Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
+
+        cfg.setTitle("Asteroids");
+        cfg.setIdleFPS(60);
+        cfg.setWindowedMode(800, 450);
+
+        new Lwjgl3Application(new Game(), cfg);
+    }
+}
