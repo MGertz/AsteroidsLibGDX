49f554e8c8d34c696f4cd8d6ce9837da918c0968
Michael Gertz <michael@ringhus.dk>
2023-02-12 12:31:41 +0100

    Create PlayState.java

---
+++ src/main/java/dk/ringhus/gamestates/PlayState.java
@@ -0,0 +1,35 @@
+package dk.ringhus.gamestates;
+
+import dk.ringhus.managers.GameStateManager;
+
+public class PlayState extends GameState{
+
+    public PlayState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
