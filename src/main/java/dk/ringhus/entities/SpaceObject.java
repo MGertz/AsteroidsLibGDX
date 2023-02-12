bbd2077464f50d952f69a43d97a5053743e65cb7
Michael Gertz <michael@ringhus.dk>
2023-02-12 13:20:50 +0100

    Update SpaceObject.java

--- src/main/java/dk/ringhus/entities/SpaceObject.java
+++ src/main/java/dk/ringhus/entities/SpaceObject.java
@@ -1,4 +1,37 @@
 package dk.ringhus.entities;
 
+import dk.ringhus.main.Game;
+
 public class SpaceObject {
+
+    // Position
+    protected float x;
+    protected float y;
+
+    // Vectror Direction traveling
+    protected float dx;
+    protected float dy;
+
+    // Direction in Radians
+    protected float radians;
+
+    // Speed
+    protected float speed;
+
+    protected float rotationSpeed;
+
+    protected int width;
+    protected int height;
+
+    protected float[] shapeX;
+    protected float[] shapeY;
+
+    protected void wrap() {
+        if(this.x < 0) x = Game.WIDTH;
+        if(this.x > Game.WIDTH) x = 0;
+        if(this.y < 0) y = Game.HEIGHT;
+        if(this.y > Game.HEIGHT) y = 0;
+    }
+
+
 }
